bec26d59bba7b88029d25556ec9aed39fc9e6bd6
Apenas-eu007 <gitxhub00@tuta.io>
2025-04-14 19:38:35 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('message-button').addEventListener('click', function() {
+    document.getElementById('message').innerText = "Você é incrível! Continue aprendendo e crescendo!";
+});
